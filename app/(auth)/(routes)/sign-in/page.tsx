e356dfd6a93188cb1f6c448771b2628ca2720f14
iqrarafiq12 <iqrak0128@gmail.com>
2023-11-12 01:43:10 +0500

    Authentication Completed

--- app/(auth)/(routes)/sign-in/page.tsx
+++
@@ -1,7 +0,0 @@
-const SigninPage = () => {
-    return ( 
-    <div>This is an Un-Protected Page</div> 
-    );
-}
- 
-export default SigninPage;
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,8 +1,11 @@
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <p className='text-3xl text-rose-500'>Welcome to the Learning Management System Iqra Rafiq</p>
-    </main>
+   <div>
+    <UserButton
+    afterSignOutUrl="/"
+    />
+   </div>
   )
 }
