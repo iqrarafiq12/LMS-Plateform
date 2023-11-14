ebcfa34bc50edb0f8bf5033c2bfddc2637370490
iqrarafiq12 <iqrak0128@gmail.com>
2023-11-14 21:59:09 +0500

    Layout Completed

--- app/(dashboard)/_components/Navbar.tsx
+++ app/(dashboard)/_components/Navbar.tsx
@@ -1,7 +1,12 @@
+import { NavbarRoutes } from "@/components/navbar-routes"
+import { MobileSideBar } from "./MobileSideBar"
+
+
 export const Navbar = () => {
     return(
         <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
-            Navbar
+       <MobileSideBar />
+   <NavbarRoutes />
         </div>
     )
 }
