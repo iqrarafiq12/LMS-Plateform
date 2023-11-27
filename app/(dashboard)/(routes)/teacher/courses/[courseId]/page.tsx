2185215252fe820a292e72b5ebc76e23d896d0c9
iqrarafiq12 <iqrak0128@gmail.com>
2023-11-27 07:37:11 -1200

    Error Solve

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,7 +1,24 @@
-const CourseIdPage = ({params}:{params:{courseId:string}}) => {
-  return (
-    <div>CourseIdPage {params.courseId}</div>
+import { db } from "@/lib/db"
+
+const CourseIdPage = async ({
+  params
+}: {
+  params: { courseId: string}
+}) => {
+
+  
+
+const course = await db.course.findUnique({
+  where: {
+    id: params.courseId
+  }
+});
+
+  return(
+    <div>
+      Course Id: {params.courseId}
+    </div>
   )
 }
 
-export default CourseIdPage
+export default CourseIdPage;
