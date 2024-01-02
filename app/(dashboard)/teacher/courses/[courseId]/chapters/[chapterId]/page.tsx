4a4dd872b8038bf959d10c46bf86e86ad5eb8b52
iqrarafiq12 <iqrak0128@gmail.com>
2024-01-02 03:50:45 -1200

    Chapter Access  Form✅

--- app/(dashboard)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -2,12 +2,13 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { ArrowLeft, LayoutDashboard } from "lucide-react";
+import { ArrowLeft, Eye, LayoutDashboard } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
 import { ChapterTitleForm } from "./_components/chapter-title-form"
 import { ChapterDescriptionForm } from "./_components/chapter-description-form";
+import { ChapterAccessForm } from "./_components/chapter-access-form";
 
 
 const ChapterIdPage = async ({
@@ -80,6 +81,20 @@ const ChapterIdPage = async ({
       courseId={params.courseId}
      chapterId={params.chapterId} 
    />
+          </div>
+
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={Eye} />
+              <h2 className="text-xl">
+                Access Settings
+              </h2>
+            </div>
+            <ChapterAccessForm 
+             initialData={chapter}
+             courseId={params.courseId}
+             chapterId={params.chapterId} 
+            />
           </div>
         </div>
       </div>
